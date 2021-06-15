3da54dbe7057577e9565611f36108710bb3685a2
Nishchith Rao <devmonk4@gmail.com>
2021-06-15 14:46:37 +0530

    check if logged in

---
+++ backend/middleware/auth.js
@@ -0,0 +1,27 @@
+const { verify } = require("jsonwebtoken");
+
+exports.isLoggedIn = (req, res, next) => {
+  let header = req.header("authorization");
+  if (!header) {
+    return res.json({
+      error: {
+        code: "auth/not-logged-in",
+      },
+    });
+  }
+  let token = header.split(" ")[1];
+  try {
+    let user = verify(token, process.env.SECRET);
+    if (user) {
+      req.user = user;
+      next();
+      return;
+    }
+  } catch (err) {
+    return res.json({
+      error: {
+        code: "auth/not-logged-in",
+      },
+    });
+  }
+};
