ed7870c8a6a8895d7e413bfff2aef9415805fb36
Nishchith Rao <devmonk4@gmail.com>
2021-06-19 21:45:48 +0530

    add months and errors messages

--- frontend/src/helpers/constants.js
+++ frontend/src/helpers/constants.js
@@ -1,3 +1,21 @@
-export const baseUrl = process.env.REAC_APP_BACKEND;
 
-export const endpoint = `${base_url}/user`;
+export const baseUrl = process.env.REACT_APP_BACKEND;
+
+export const userEndpoint = `/user`;
+
+export const authEndpoint = `/auth`;
+
+export const secret = process.env.REACT_APP_SECRET;
+
+export const possibleErrors = [
+    {
+        code: "auth/no-account-found",
+        "message": "No account found, please try with a different email"
+    },{
+        code: "auth/invalid-password",
+        "message": "The Password is incorrect, please try again"
+    }
+]
+
+export const months = ["January","February","March","April","May"
+,"June","July","August","September","October","November","December"];
