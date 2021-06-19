d96b3b631b8a83977497d16ab039ece0c95d036e
Nishchith Rao <devmonk4@gmail.com>
2021-06-19 21:51:47 +0530

    create store

---
+++ frontend/src/store/index.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import {isLoggedIn,logState} from './middlewares';
+import createSagaMiddleware from 'redux-saga';
+import main from './modules';
+import { useSelector } from "react-redux";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const allMiddlewares = applyMiddleware(isLoggedIn,logState,sagaMiddleware);
+
+const enhancer = compose(allMiddlewares,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+const store = createStore(main.reducers(),allMiddlewares);
+
+export const actions = main.actions;
+
+sagaMiddleware.run(main.saga());
+
+export default store;
