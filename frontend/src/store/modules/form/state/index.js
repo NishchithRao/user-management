18d025246537e40f7d3c710f56d15fbffed1b9a7
Nishchith Rao <devmonk4@gmail.com>
2021-06-20 12:17:50 +0530

    put initial state into seperate files

---
+++ frontend/src/store/modules/form/state/index.js
@@ -0,0 +1,20 @@
+const initialState = {
+    firstName: '',
+    lastName: '',
+    password: '',
+    email: '',
+    dob: {
+        dd: '',
+        mm: '',
+        yy: ''
+    },
+    gender: '',
+    address: {
+        line1: '',
+        state: '',
+        city: '',
+        pincode: '',
+    }
+}
+
+export default initialState;
---
+++ frontend/src/store/modules/user/state/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+    users: [],
+    profile: {},
+    anotherUser: {},
+    isLoggedIn: false,
+    isLoading: false,
+    isSuccess: false,
+    error: "12"
+  }
+
+  export default initialState;
