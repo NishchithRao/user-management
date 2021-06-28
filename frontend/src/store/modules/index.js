7c1898ecb3f44c5a889dedc5f69223034e30a45d
Nishchith Rao <devmonk4@gmail.com>
2021-06-29 03:40:23 +0530

    export modules

--- frontend/src/store/modules/index.js
+++ frontend/src/store/modules/index.js
@@ -2,14 +2,14 @@ import { combineReducers } from "redux";
 import formModule from './form';
 import userModule from './user';
 
-const formReducer = formModule.getReducers();
-const userReducer = userModule.getReducers();
+const form = formModule.getReducers();
+const user = userModule.getReducers();
 const saga = userModule.saga;
 
 const main = {
-    reducers: () => combineReducers({formReducer,userReducer}),
+    reducers: () => combineReducers({form,user}),
     saga: () => saga,
-    actions: () => {userModule.getActions(),formModule.getActions()}
+    actions: () => ({user:userModule.getActions(),form:formModule.getActions()})
 }
 
 
