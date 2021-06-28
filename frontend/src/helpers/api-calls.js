79d458e0303014fb3069dcbf9ddef10d04eef651
Nishchith Rao <devmonk4@gmail.com>
2021-06-29 03:34:45 +0530

    write api calls to backend

--- frontend/src/helpers/api-calls.js
+++ frontend/src/helpers/api-calls.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import { imgInstance } from "../axios_instances/imgInstance";
 import { notTokenInstance } from "../axios_instances/notTokenInstance";
 import { tokenInstance } from "../axios_instances/tokenInstance";
 
@@ -9,5 +9,10 @@ export const plainPostCall = (url,data) => {
 
 export const authorizedPostCall = (url,data) => tokenInstance.post(url,data);
 
-
 export const authorizedGetCall = url => tokenInstance.get(url);
+
+export const authorizedPutCall = (url,data) => tokenInstance.put(url,data);
+
+export const authorizedSetImgCall = (url,data) => imgInstance.put(url,data);
+
+export const authorizedDeleteCall = (url) => tokenInstance.delete(url);
